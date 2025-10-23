45d5abf7b3a0ad8d2abeb99dc8d0ebb0ebe8b7f7
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-23 23:48:53 +0100

    Set Up GraphQL Endpoint

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'grapene_django',
+    'graphene_django',
     'crm',
 ]
 
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    'SCHEMA': 'alx_backend_graphql.schema'
+}
