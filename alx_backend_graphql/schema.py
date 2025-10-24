1e9f3e3ff2f63a02c0e52d3d49cf8b08377e852e
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-24 01:34:31 +0100

    Connect the GraphQL Endpoint

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import graphene
-from crm import CRMQuery
+from crm.schema import CRMQuery
 
 class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value='Hello, GraphQL!')
