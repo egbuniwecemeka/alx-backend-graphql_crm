959d1286036544f777f7da92131e67cf07b43ec6
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-24 02:15:01 +0100

    Mutations defined

--- crm/schema.py
+++ crm/schema.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+import graphene
+
 class CRMQuery():
-    pass
+    hello = graphene.String(default_value='Hello, GraphQL!')
+
+
+class CreateCustomer(graphene.Mutation):
+    ok = graphene.Boolean()
+    customer_name = graphene.String()
+    customer_email = graphene.String()
+    customer_phone = graphene.String()
+
+    class Arguments():
+        name = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, name, email):
+        ok = True
+        return CreateCustomer(customer_name=name, customer_email=email, ok=ok)
