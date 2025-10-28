580c20b37a3e314daeadd3fd24c79adb48b251fe
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-28 09:02:00 +0100

    Customer class added

--- crm/models.py
+++ crm/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+
+    def __str__(self, name):
+        return self.name
