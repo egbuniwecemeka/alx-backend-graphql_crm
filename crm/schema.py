e3448b5f47bd84fb9fcf1957121b934a961d43b8
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-29 20:26:42 +0100

    Task 2: Implement Complex GraphQL Mutations for CRM

--- crm/schema.py
+++ crm/schema.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import graphene
-from .models import Customers
+from .models import Customer
 import re
 
 
@@ -19,19 +19,19 @@ class Query(graphene.ObjectType):
 
 
 class CreateCustomer(graphene.Mutation):
+    # Inputs
     ok = graphene.Boolean()
-    customer_name = graphene.String()
-    customer_email = graphene.String()
-    customer_phone = graphene.String()
+    customer = graphene.Field(CustomerType)
 
-    class Arguments():
+    class Arguments:
         name = graphene.String(required=True)
         email = graphene.String(required=True)
         phone = graphene.String(required=False)
 
+    # Validations
     def mutate(self, name, email, phone):
         # Email uniqueness
-        if Customers.objects.filter(email=email).exists:
+        if Customer.objects.filter(email=email).exists():
             raise Exception('Email already exists')
 
         # Validate the phone number
@@ -39,24 +39,79 @@ class CreateCustomer(graphene.Mutation):
             raise Exception('Phone number format: +1234567890 or 123-456-7890')
         
         # Save to DB
-        customer = Customers(name=name, email=email, phone=phone)
+        customer = Customer(name=name, email=email, phone=phone)
         customer.save()
 
         return CreateCustomer(
             ok = True,
-            customer_name=customer.name,
-            customer_email=customer.email,
-            customer_phone=customer.phone
+            customer=customer
         )
 
 
-class BulkCreateCustomers():
+class CustomerInput(graphene.InputObjectType):
+    """ Customer structure """
+    name = graphene.String(required=True)
+    email = graphene.String(required=True)
+    phone = graphene.String(required=False)
+
+
+class BulkCreateCustomers(graphene.Mutation):
+    """ """
     ok = graphene.Boolean()
-    customer_list = 
+    customer_list = graphene.List(
+        graphene.NonNull(graphene.String)
+    )
+
+    class Argument:
+        """  """
+        ok = graphene.Boolean()
+        customer_list = graphene.List(
+            graphene.NotNull(CustomerInput, required=True)
+        )
+    
+    def mutate(self, info, customer_list):
+        """ """
+        created_customers = []
+
+        for customer_info in customer_list:
+            name = customer_info.name
+            email = customer_info.email
+            phone = customer_info.phone
+        
+        # Email validation
+        if Customer.object.filter(email=email).exists():
+            raise Exception(f'{email} already exists')
+        
+        # Phone number validation 
+        if phone and re.match(r'^\+?\d{10, 15}\d{3}-\d{3}-d{4}$'):
+            raise Exception(f'Invalid phone no. {name}: Input eg +123456789 or +123-456-7890')
+        
+        # Save to DB
+        customer = Customer(name=name, email=email, phone=phone)
+        customer.save()
+
+        created_customers.append(customer)
+
+        return BulkCreateCustomers(ok=True, customer_list=created_customers)
+    
+
+class CreateProduct(graphene.Mutation):
+    """ """
+    ok = graphene.Boolean()
+    name = graphene.String()
+    price = graphene.Float()
+    stock = graphene.Int()
 
 
-class CreateProduct():
-    pass
+    class Arguments:
+        ok = graphene.Boolean()
+        name = graphene.String(required=True)
+        price = graphene.Float(required=True)
+        stock = graphene.Int(required=False, default_value=0)
+    
+    def mutate(self, info, price, stock):
+        if price >= 0 and stock > 0:
+            return Exception('Price and stock must be positive')
 
 
 class CreateOrder():
