00c1c9fb2d88b16ee0b285a75229220b5887365b
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-29 21:21:09 +0100

    Add Filtering

--- crm/filters.py
+++ crm/filters.py
@@ -2,9 +2,13 @@
 """ Implementing filtering functionalities """
 
 import django_filters
+from .models import Customer
 
 
 class CustomerFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     email = django_filters.CharFilter(field_name='email', lookup_expr='icontains')
     created_at = django_filters.DateFromToRangeFilter()
+
+    class Meta:
+        model = Customer
