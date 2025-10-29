c8200ac8550d4fcefd0d036ef23f22a61d6f8721
egbuniwecemeka <egbuniwecemeka@gmail.com>
2025-10-29 20:59:38 +0100

    Add Filtering

--- crm/filters.py
+++ crm/filters.py
@@ -4,6 +4,7 @@
 import django_filters
 
 
-class CustomerFilter:
+class CustomerFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     email = django_filters.CharFilter(field_name='email', lookup_expr='icontains')
+    created_at = django_filters.DateFromToRangeFilter()
